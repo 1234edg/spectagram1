210f3b1c7b2e1a4bec15e3cf5f5c6edef5e16a78
1234edg <86652346+1234edg@users.noreply.github.com>
2022-04-25 15:55:52 +0530

    Add files via upload

---
+++ Navigation/DrawerNavigator.js
@@ -0,0 +1,11 @@
+import{createDrawerNavigator} from "@react-navigation/drawer";
+import TabNavigator from "./TabNavigator";
+import profile from "../screens/Profile";
+
+const Drawer= createDrawerNavigator();
+const DrawerNavigator=() => {return (
+    <Drawer.Navigator>
+        <Drawer.screen name="Home" component={TabNavigator}/>
+        <Drawer.screen name="Profile"  component={Profile}/>
+    </Drawer.Navigator>);}
+    export default DrawerNavigator;
